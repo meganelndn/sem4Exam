447c4836bed650b3e0df70a530b2243264ca43ab
megane <megane.londono@hotmail.com>
2020-11-24 13:52:42 +0100

    btns working in sync with form validation

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 window.addEventListener("DOMContentLoaded", init);
 
-/* import { gsap } from "gsap"; */
-
 function init() {
     fetchData();
     setUpBooking();
@@ -36,7 +34,7 @@ function fetchData(){
 }
 
 function setUpBooking(){
-    // set up book now window
+    /* ------------ set up book now window ----------- */
     document.getElementById("bookBtn").addEventListener("click", function(){
         let bookOverlay = document.getElementById("bookingOverlay");
         bookOverlay.classList.toggle("showOverlay");
@@ -44,11 +42,11 @@ function setUpBooking(){
 
     let tourInput = document.querySelector("select[name='tour']");
     tourInput.addEventListener("change", e => {
-        if (e.target.value == "landmarks") {
+        if (e.target.value === "landmarks") {
             document.querySelector(".landmarks").classList.remove("show");
             document.querySelector(".hidden-gems").classList.add("show");
             document.querySelector(".diana").classList.add("show");
-        } else if (e.target.value == "hidden-gems") {
+        } else if (e.target.value === "hidden-gems") {
             document.querySelector(".hidden-gems").classList.remove("show");
             document.querySelector(".landmarks").classList.add("show");
             document.querySelector(".diana").classList.add("diana");
@@ -59,7 +57,7 @@ function setUpBooking(){
         }
     })
 
-    // enable buttons
+    /* ------------ enable next/previous btn ----------- */
     var counter = 1, step = "step";
 
     document.querySelector(".next").addEventListener('click', function () {
@@ -75,7 +73,9 @@ function setUpBooking(){
     step = ".step" + counter; // step is the class and we are appending counter with step so that it looks like the same class in the given class(like counter 1 means step1)
 
     document.querySelector(step).classList.remove("show");
-});
+    }); 
+
+    var counter = 1, step = "step";
 
     document.querySelector(".previous").addEventListener('click', function () {
 
@@ -85,6 +85,7 @@ function setUpBooking(){
         document.querySelector(step).classList.add("show");
     }
     counter--;
+    
     if (counter < 1) {
         counter = 1;
     }
@@ -92,6 +93,72 @@ function setUpBooking(){
 
     document.querySelector(step).classList.remove("show");
 });
+
+    formValidation();
+}
+
+function formValidation() {
+    /* ------------ form & elements ----------- */
+    const form1 = document.querySelector(".availability");
+    window.form1 = form1;
+    const elements = form1.elements;
+    window.elements = elements;
+
+    /* --------- delete default validation ------- */
+    form1.setAttribute("novalidate", true);
+
+    /* ------------ custom validation ------------ */
+    document.querySelector(".next").addEventListener("click", (e) => {
+        e.preventDefault();
+
+        // 1. select all inputs
+        const formElements = form1.querySelectorAll("input");
+
+        /* ------------ date ------------ */
+        if (form1.checkValidity()) {
+            // loop through form elements and check if are valid or not
+            formElements.forEach((elements) => {
+                elements.classList.remove("invalid");
+
+                // enable "next" btn when form is valid
+                var counter = 1, step = "step";
+                step = ".step" + counter;
+                if (counter <= 5) {
+                    document.querySelector(step).classList.add("show");
+                }
+                counter++;
+                if (counter > 5) {
+                    counter = 5;
+                }
+                step = ".step" + counter; // step is the class and we are appending counter with step so that it looks like the same class in the given class(like counter 1 means step1)
+
+                document.querySelector(step).classList.remove("show");
+            });
+
+            console.log("form is valid");
+
+        } else {
+            formElements.forEach((elements) => {
+                // if elements invalid, add error messages
+                if (!elements.checkValidity()) {
+                    elements.classList.add("invalid");
+                        document.querySelector(".date-err").classList.add("invalid");
+                        document.querySelector(".date-err").style.display = "block";
+                        document.querySelector(".tour-err").classList.add("invalid");
+                        document.querySelector(".tour-err").style.display = "block";
+                        document.querySelector(".passengers-err").classList.add("invalid");
+                        document.querySelector(".passengers-err").style.display = "block";
+                        document.querySelector(".time-err").classList.add("invalid");
+                        document.querySelector(".time-err").style.display = "block"; 
+                    
+                    // if form invalid, cannot continue
+                    if (!form1.checkValidity()) {
+                        console.log("form is invalid");
+                    } 
+                }
+            })
+        } 
+    })
 }
 
 function showLandingPage(home) {
@@ -100,11 +167,11 @@ function showLandingPage(home) {
     const introCopy = introTemplate.cloneNode(true);
 
     // 2. text content
-    // HERO IMAGE
+    /* ------------ hero image ----------- */
     introCopy.querySelector(".heroImg").src = home[0].hero_video.guid;
-    // BANNER
+    /* -------------- banner ------------- */
     introCopy.querySelector(".banner").src = home[0].banner.guid;
-    // INTRO TEXT
+    /* ------------ intro text ----------- */
     introCopy.querySelector(".introTitle").textContent = home[0].social_sailing_in_copenhagen;
 
     // 3. append
@@ -211,15 +278,15 @@ function showContact(contact) {
     const templateC = document.querySelector(".contactTemplate").content;
     const contactCopy = templateC.cloneNode(true);
     // 2. text content
-    // SEASON OPENINGS
+    /* ------------ season openings ----------- */
     contactCopy.querySelector(".contactText").textContent = contact[2].openings;
-    // DEPARTURES/ARRIVALS
+    /* ---------- departures/arrivals ---------- */
     contactCopy.querySelector(".contactText2").textContent = contact[1].location; 
-    // CONTACT US
+    /* -------------- contact us --------------- */
     contactCopy.querySelector(".contactTitle3").textContent = contact[3].title.rendered;
     contactCopy.querySelector(".contactLink1").textContent = `E-mail: info@heycaptain.dk`; 
     contactCopy.querySelector(".contactLink2").textContent = `Phone: ${contact[3].contact_phone}`; 
-    // FOLLOW US
+    /* --------------- follow us --------------- */
     contactCopy.querySelector(".contactTitle4").textContent = contact[0].title.rendered;
     // 3. append
     document.querySelector("#bottomNavigation").appendChild(contactCopy);
