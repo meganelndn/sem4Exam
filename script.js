2e40d43e5f62264e9c589ff4175429eabead0158
megane <megane.londono@hotmail.com>
2020-11-25 09:20:53 +0100

    form validation not done

--- script.js
+++ script.js
@@ -64,8 +64,18 @@ function formValidation() {
     /* ------------ form & elements ----------- */
     const form1 = document.querySelector(".availability");
     window.form1 = form1;
-    const elements = form1.elements;
-    window.elements = elements;
+    const elements1 = form1.elements;
+    window.elements1 = elements1;
+
+    /* const form2 = document.querySelector(".personalData");
+    window.form2 = form2;
+    const elements2 = form2.elements;
+    window.elements2 = elements2;
+
+    const form4 = document.querySelector(".payment");
+    window.form4 = form4;
+    const elements4 = form4.elements;
+    window.elements4 = elements4; */
 
     /* --------- delete default validation ------- */
     form1.setAttribute("novalidate", true);
@@ -75,51 +85,24 @@ function formValidation() {
         e.preventDefault();
 
         // 1. select all inputs
-        const formElements = form1.querySelectorAll("input, select");
+        const formElements1 = form1.querySelectorAll("input, select");
+        /* const formElements2 = form2.querySelectorAll("input, select");
+        const formElements4 = form4.querySelectorAll("input, select"); */
 
+        /* ------------ availability form ------------ */
         if (form1.checkValidity()) {
             console.log("form is valid");
             
             // loop through form elements and check if are valid or not
-            formElements.forEach((el) => {
+            formElements1.forEach((el) => {
                 if (el.checkValidity()) { 
                     el.classList.add("valid");
                 }
 
-                // enable "next" btn when form is valid
-                var counter = 1, step = "step";
-                step = ".step" + counter;
-                if (counter <= 5) {
-                    document.querySelector(step).classList.add("show");
-                }
-                counter++;
-                if (counter > 5) {
-                    counter = 5;
-                }
-                step = ".step" + counter; // step is the class and we are appending counter with step so that it looks like the same class in the given class(like counter 1 means step1)
-
-                document.querySelector(step).classList.remove("show");
-
-                // enable "previous" btn when form is valid
-                document.querySelector(".previous").addEventListener('click', function () {
-
-                    if (counter > 1) { // we don't want to remove the first step, it will always be shown
-                        step = ".step" + counter;
-                
-                        document.querySelector(step).classList.add("show");
-                    }
-                    counter--;
-                    
-                    if (counter < 1) {
-                        counter = 1;
-                    }
-                    step = ".step" + counter;
-                
-                    document.querySelector(step).classList.remove("show");
-                });
+                nextAndPrevious();
             });
         } else {
-            formElements.forEach((el) => {
+            formElements1.forEach((el) => {
                 
                 if (!el.checkValidity()) {
                     console.log("form is invalid");
@@ -127,43 +110,43 @@ function formValidation() {
                     el.classList.add("invalid");
 
                     /* ------------ date ------------ */
-                    elements.date.addEventListener("change", () => {                
-                        if (elements.date.value != "") {
-                            elements.date.classList.add("valid");
-                            elements.date.classList.remove("invalid");
+                    elements1.date.addEventListener("change", () => {                
+                        if (elements1.date.value != "") {
+                            elements1.date.classList.add("valid");
+                            elements1.date.classList.remove("invalid");
                         } else {                
-                            elements.date.classList.remove("valid");
-                            elements.date.classList.add("invalid");
+                            elements1.date.classList.remove("valid");
+                            elements1.date.classList.add("invalid");
                         }
                     })
                     /* -------------- tour ------------ */
-                    elements.tour.addEventListener("change", () => {                
-                        if (elements.tour.value != "") {
-                            elements.tour.classList.add("valid");
-                            elements.tour.classList.remove("invalid");
+                    elements1.tour.addEventListener("change", () => {                
+                        if (elements1.tour.value != "") {
+                            elements1.tour.classList.add("valid");
+                            elements1.tour.classList.remove("invalid");
                         } else {                
-                            elements.tour.classList.remove("valid");
-                            elements.tour.classList.add("invalid");
+                            elements1.tour.classList.remove("valid");
+                            elements1.tour.classList.add("invalid");
                         }
                     })
                     /* ------------ passengers ------------ */
-                    elements.passengers.addEventListener("change", () => {                
-                        if (elements.passengers.value != "") {
-                            elements.passengers.classList.add("valid");
-                            elements.passengers.classList.remove("invalid");
+                    elements1.passengers.addEventListener("change", () => {                
+                        if (elements1.passengers.value != "") {
+                            elements1.passengers.classList.add("valid");
+                            elements1.passengers.classList.remove("invalid");
                         } else {                
-                            elements.passengers.classList.remove("valid");
-                            elements.passengers.classList.add("invalid");
+                            elements1.passengers.classList.remove("valid");
+                            elements1.passengers.classList.add("invalid");
                         }
                     })
                     /* ------------ date & time ------------ */
-                    elements.time.addEventListener("change", () => {                
-                        if (elements.time.value != "") {
-                            elements.time.classList.add("valid");
-                            elements.time.classList.remove("invalid");
+                    elements1.time.addEventListener("change", () => {                
+                        if (elements1.time.value != "") {
+                            elements1.time.classList.add("valid");
+                            elements1.time.classList.remove("invalid");
                         } else {                
-                            elements.time.classList.remove("valid");
-                            elements.time.classList.add("invalid");
+                            elements1.time.classList.remove("valid");
+                            elements1.time.classList.add("invalid");
                         }
                     })
 
@@ -172,7 +155,215 @@ function formValidation() {
                     }
                 })
             }
-        })
+
+        /* ------------ personal data form ------------ */
+        // if (form2.checkValidity()) {
+        //     console.log("form is valid");
+            
+        //     // loop through form elements and check if are valid or not
+        //     formElements2.forEach((el) => {
+        //         if (el.checkValidity()) { 
+        //             el.classList.add("valid");
+        //         }
+
+        //         /* nextAndPrevious(); */
+        //     });
+        // } else {
+        //     formElements2.forEach((el) => {
+                
+        //         if (!el.checkValidity()) {
+        //             console.log("form is invalid");
+
+        //             el.classList.add("invalid");
+
+        //             /* ------------ date ------------ */
+        //             elements2.name.addEventListener("keyup", () => {                
+        //                 if (elements2.name.value != "") {
+        //                     elements2.name.classList.add("valid");
+        //                     elements2.name.classList.remove("invalid");
+        //                 } else {                
+        //                     elements2.name.classList.remove("valid");
+        //                     elements2.name.classList.add("invalid");
+        //                 }
+        //             })
+        //             /* -------------- tour ------------ */
+        //             elements2.email.addEventListener("keyup", () => {                
+        //                 if (elements2.email.value != "") {
+        //                     elements2.email.classList.add("valid");
+        //                     elements2.email.classList.remove("invalid");
+        //                 } else {                
+        //                     elements2.email.classList.remove("valid");
+        //                     elements2.email.classList.add("invalid");
+        //                 }
+        //             })
+        //             /* ------------ passengers ------------ */
+        //             elements2.phone.addEventListener("keyup", () => {                
+        //                 if (elements2.phone.value != "") {
+        //                     elements2.phone.classList.add("valid");
+        //                     elements2.phone.classList.remove("invalid");
+        //                 } else {                
+        //                     elements2.phone.classList.remove("valid");
+        //                     elements2.phone.classList.add("invalid");
+        //                 }
+        //             })
+        //             /* ------------ date & time ------------ */
+        //             elements2.accommodation.addEventListener("change", () => {                
+        //                 if (elements2.accommodation.value != "") {
+        //                     elements2.accommodation.classList.add("valid");
+        //                     elements2.accommodation.classList.remove("invalid");
+        //                 } else {                
+        //                     elements2.accommodation.classList.remove("valid");
+        //                     elements2.accommodation.classList.add("invalid");
+        //                 }
+        //             })
+        //         } else {
+        //             el.classList.remove("invalid");
+        //         }
+        //     })
+        // }
+
+        /* ------------ order overview ------------ */
+        // if (form3.checkValidity()) {
+        //     console.log("form is valid");
+
+        //     // loop through form elements and check if are valid or not
+        //     formElements3.forEach((el) => {
+        //         if (el.checkValidity()) { 
+        //             el.classList.add("valid");
+        //         }
+
+        //         /* nextAndPrevious(); */
+        //     });
+        // } else {
+        //     formElements3.forEach((el) => {
+                
+        //         if (!el.checkValidity()) {
+        //             console.log("form is invalid");
+
+        //             el.classList.add("invalid");
+
+        //             /* ------------ date ------------ */
+        //             elements3.terms.addEventListener("change", () => {                
+        //                 if (elements3.terms.value != "") {
+        //                     elements3.terms.classList.add("valid");
+        //                     elements3.terms.classList.remove("invalid");
+        //                 } else {                
+        //                     elements3.terms.classList.remove("valid");
+        //                     elements3.terms.classList.add("invalid");
+        //                 }
+        //             })
+        //         } else {
+        //             el.classList.remove("invalid");
+        //         }
+        //     })
+        // }
+
+        /* ------------ payment form ------------ */
+        // if (form4.checkValidity()) {
+        //     console.log("form is valid");
+            
+        //     // loop through form elements and check if are valid or not
+        //     formElements4.forEach((el) => {
+        //         if (el.checkValidity()) { 
+        //             el.classList.add("valid");
+        //         }
+
+        //        /*  nextAndPrevious(); */
+        //     });
+        // } else {
+            
+        //     formElements4.forEach((el) => {
+                
+        //         if (!el.checkValidity()) {
+        //             console.log("form is invalid");
+
+        //             el.classList.add("invalid");
+
+        //             /* ------------ date ------------ */
+        //             elements4.cardNumber.addEventListener("keyup", () => {                
+        //                 if (elements4.cardNumber.value != "") {
+        //                     elements4.cardNumber.classList.add("valid");
+        //                     elements4.cardNumber.classList.remove("invalid");
+        //                 } else {                
+        //                     elements4.cardNumber.classList.remove("valid");
+        //                     elements4.cardNumber.classList.add("invalid");
+        //                 }
+        //             })
+        //             /* -------------- tour ------------ */
+        //             elements4.cardName.addEventListener("keyup", () => {                
+        //                 if (elements4.cardName.value != "") {
+        //                     elements4.cardName.classList.add("valid");
+        //                     elements4.cardName.classList.remove("invalid");
+        //                 } else {                
+        //                     elements4.cardName.classList.remove("valid");
+        //                     elements4.cardName.classList.add("invalid");
+        //                 }
+        //             })
+        //             /* ------------ passengers ------------ */
+        //             elements4.expiryDate.addEventListener("keyup", () => {                
+        //                 if (elements4.expiryDate.value != "") {
+        //                     elements4.expiryDate.classList.add("valid");
+        //                     elements4.expiryDate.classList.remove("invalid");
+        //                 } else {                
+        //                     elements4.expiryDate.classList.remove("valid");
+        //                     elements4.expiryDate.classList.add("invalid");
+        //                 }
+        //             })
+        //             /* ------------ date & time ------------ */
+        //             elements4.ccv.addEventListener("keyup", () => {                
+        //                 if (elements4.ccv.value != "") {
+        //                     elements4.ccv.classList.add("valid");
+        //                     elements4.ccv.classList.remove("invalid");
+        //                 } else {                
+        //                     elements4.ccv.classList.remove("valid");
+        //                     elements4.ccv.classList.add("invalid");
+        //                 }
+        //             })
+        //         } else {
+        //             el.classList.remove("invalid");
+        //         }
+        //     })
+        //  }
+    })  
+}
+
+function nextAndPrevious() {
+    // enable "next" btn when form is valid
+    var counter = 1, step = "step";
+    step = ".step" + counter;
+    
+    if (counter <= 5) {
+        document.querySelector(step).classList.add("show");
+    }
+    
+    counter++;
+    if (counter > 5) {
+        counter = 5;
+    }
+    
+    step = ".step" + counter; // step is the class and we are appending counter with step so that it looks like the same class in the given class(like counter 1 means step1)
+
+    document.querySelector(step).classList.remove("show");
+
+    // enable "previous" btn when form is valid
+    document.querySelector(".previous").addEventListener('click', function () {
+
+    if (counter > 1) {
+        step = ".step" + counter;
+                
+        document.querySelector(step).classList.add("show");
+    }
+    
+    counter--;
+                    
+    if (counter < 1) {
+        counter = 1;
+    }
+    
+    step = ".step" + counter;
+                
+    document.querySelector(step).classList.remove("show");
+    });
 }
 
 function showLandingPage(home) {
