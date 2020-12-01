23903f62d4e566f19be45ed6b7f649afcc560ad8
megane <megane.londono@hotmail.com>
2020-12-01 13:53:20 +0100

    cleaned up gallery code

--- script.js
+++ script.js
@@ -262,96 +262,94 @@ function showGalleryPage(gallery) {
 
     document.querySelector("#gallery").appendChild(galleryCopy);
 
-/*---------------Album Cover Slider---------------*/
-//start added by Chase
-var a = document.getElementsByTagName("a");
-var cfImg = document.getElementsByClassName("coverflow__image")
+    /*---------------Image Carousel---------------*/
+    //start added by Chase
+    var a = document.getElementsByTagName("a");
+    var cfImg = document.getElementsByClassName("coverflow__image")
 
-var scaleI = 0;
-for (scaleI; scaleI < a.length; scaleI++) {
-  if (scaleI === 3) {
-    continue;
-  } else {
-    a[scaleI].style.cursor = "default";
-    a[scaleI].addEventListener("click", prevDef);
-  }
-}
-
-function prevDef(e) {
-  e.preventDefault();
-}
-
-function forScale(coverflowPos) {
-  for (scaleI = 0; scaleI < a.length; scaleI++) {
-    a[scaleI].style.cursor = "default";
-    a[scaleI].addEventListener("click", prevDef);
-  }
-  for (scaleI = 0; scaleI < cfImg.length; scaleI++) {
-    if (cfImg[scaleI].getAttribute("data-coverflow-index") == coverflowPos) {
-      cfImg[scaleI].parentElement.style.cursor = "pointer";
-      cfImg[scaleI].parentElement.removeEventListener("click", prevDef);
-    }
-  }
-}
-//end added by Chase
-
-function setupCoverflow(coverflowContainer) {
-  var coverflowContainers;
-
-  if (typeof coverflowContainer !== "undefined") {
-    if (Array.isArray(coverflowContainer)) {
-      coverflowContainers = coverflowContainer;
+    var scaleI = 0;
+    for (scaleI; scaleI < a.length; scaleI++) {
+        if (scaleI === 3) {
+        continue;
     } else {
-      coverflowContainers = [coverflowContainer];
+        a[scaleI].style.cursor = "default";
+        a[scaleI].addEventListener("click", prevDef);
     }
-  } else {
-    coverflowContainers = Array.prototype.slice.apply(document.getElementsByClassName('coverflow'));
-  }
+    }
 
-  coverflowContainers.forEach(function(containerElement) {
-    var coverflow = {};
-    var prevArrows, nextArrows;
+    function prevDef(e) {
+        e.preventDefault();
+    }
 
-    //capture coverflow elements
-    coverflow.container = containerElement;
-    coverflow.images = Array.prototype.slice.apply(containerElement.getElementsByClassName('coverflow__image'));
-    coverflow.position = Math.floor(coverflow.images.length / 2) + 1;
+    function forScale(coverflowPos) {
+        for (scaleI = 0; scaleI < a.length; scaleI++) {
+            a[scaleI].style.cursor = "default";
+            a[scaleI].addEventListener("click", prevDef);
+        }
+        for (scaleI = 0; scaleI < cfImg.length; scaleI++) {
+            if (cfImg[scaleI].getAttribute("data-coverflow-index") == coverflowPos) {
+            cfImg[scaleI].parentElement.style.cursor = "pointer";
+            cfImg[scaleI].parentElement.removeEventListener("click", prevDef);
+        }
+    }
+    }
 
-    //set indicies on images
-    coverflow.images.forEach(function(coverflowImage, i) {
-      coverflowImage.dataset.coverflowIndex = i + 1;
-    });
+    function setupCoverflow(coverflowContainer) {
+        var coverflowContainers;
 
-    //set initial position
-    coverflow.container.dataset.coverflowPosition = coverflow.position;
+        if (typeof coverflowContainer !== "undefined") {
+        if (Array.isArray(coverflowContainer)) {
+            coverflowContainers = coverflowContainer;
+        } else {
+            coverflowContainers = [coverflowContainer];
+        }
+        } else {
+        coverflowContainers = Array.prototype.slice.apply(document.getElementsByClassName('coverflow'));
+    }
 
-    //get prev/next arrows
-    prevArrows = Array.prototype.slice.apply(coverflow.container.getElementsByClassName("prev-arrow"));
-    nextArrows = Array.prototype.slice.apply(coverflow.container.getElementsByClassName("next-arrow"));
+    coverflowContainers.forEach(function(containerElement) {
+        var coverflow = {};
+        var prevArrows, nextArrows;
 
-    //add event handlers
-    function setPrevImage() {
-      coverflow.position = Math.max(1, coverflow.position - 1);
-      coverflow.container.dataset.coverflowPosition = coverflow.position;
-      //call the functin forScale added
-      forScale(coverflow.position);
+        //capture coverflow elements
+        coverflow.container = containerElement;
+        coverflow.images = Array.prototype.slice.apply(containerElement.getElementsByClassName('coverflow__image'));
+        coverflow.position = Math.floor(coverflow.images.length / 2) + 1;
+
+        //set indicies on images
+        coverflow.images.forEach(function(coverflowImage, i) {
+        coverflowImage.dataset.coverflowIndex = i + 1;
+        });
+
+        //set initial position
+        coverflow.container.dataset.coverflowPosition = coverflow.position;
+
+        //get prev/next arrows
+        prevArrows = Array.prototype.slice.apply(coverflow.container.getElementsByClassName("prev-arrow"));
+        nextArrows = Array.prototype.slice.apply(coverflow.container.getElementsByClassName("next-arrow"));
+
+        //add event handlers
+        function setPrevImage() {
+        coverflow.position = Math.max(1, coverflow.position - 1);
+        coverflow.container.dataset.coverflowPosition = coverflow.position;
+        //call the functin forScale added
+        forScale(coverflow.position);
     }
 
     function setNextImage() {
-      coverflow.position = Math.min(coverflow.images.length, coverflow.position + 1);
-      coverflow.container.dataset.coverflowPosition = coverflow.position;
-      //call the function Chase added
-      forScale(coverflow.position);
+        coverflow.position = Math.min(coverflow.images.length, coverflow.position + 1);
+        coverflow.container.dataset.coverflowPosition = coverflow.position;
+        //call the function Chase added
+        forScale(coverflow.position);
     }
 
     function jumpToImage(evt) {
-      coverflow.position = Math.min(coverflow.images.length, Math.max(1, evt.target.dataset.coverflowIndex));
-      coverflow.container.dataset.coverflowPosition = coverflow.position;
-      //start added by Chase
-      setTimeout(function() {
-        forScale(coverflow.position);
-      }, 1);
-      //end added by Chase
+        coverflow.position = Math.min(coverflow.images.length, Math.max(1, evt.target.dataset.coverflowIndex));
+        coverflow.container.dataset.coverflowPosition = coverflow.position;
+        //start added by Chase
+        setTimeout(function() {
+            forScale(coverflow.position);
+        }, 1);
     }
 
     function onKeyPress(evt) {
@@ -374,8 +372,8 @@ function setupCoverflow(coverflowContainer) {
       image.addEventListener('click', jumpToImage);
     });
     window.addEventListener('keyup', onKeyPress);
-  });
-}
+    });
+    }
 
 setupCoverflow();
 }
