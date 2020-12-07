017060640194f00beae39a3da86e8d50d8cd0a50
megane <megane.londono@hotmail.com>
2020-12-07 12:08:59 +0100

    tried smooth scroll

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ function sendNewsletter(){
     document.querySelector(".overlay-content2").classList.remove("show");
     document.querySelector(".overlay-content").classList.add("hideOverlay");
     document.querySelector("#newsletterBtn").addEventListener("click", function(){
-        location.reload(); 
+        /* location.reload();  */
     })
 }
 
