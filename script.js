6c934bcdee47dce92b9a1a4a85ea263f54d57df2
megane <megane.londono@hotmail.com>
2020-12-07 14:25:48 +0100

    widget npm module installed, react code not working yet

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function init() {
     smoothScroll();
     setUpNewsletter();
     setUpBooking();
-    weatherApp();
+    /* weatherApp(); */
 }
 
 function fetchData(){
@@ -39,7 +39,7 @@ function fetchData(){
 
 import { checkValidation, closeForm, postSubscription } from "./modules/bookingSteps";
 import { carouselEffect } from "./modules/imageCarousel";
-import { weatherCard } from "./modules/weatherCard.js";
+/* import { weather } from "./modules/like_button.js"; */
 
 let completedForm;
 
@@ -374,53 +374,53 @@ function showContact(contact) {
     document.querySelector("#bottomNavigation").appendChild(contactCopy);
 }
 
-function weatherApp() {
-    /* https://codepen.io/tutsplus/pen/gObLaEP  */
+// function weatherApp() {
+//     /* https://codepen.io/tutsplus/pen/gObLaEP  */
 
-    /* SEARCH BY USING A CITY NAME OR A COMMA-SEPARATED CITY NAME ALONG WITH THE COUNTRY CODE */
-    const form = document.querySelector(".top-banner form");
-    const input = document.querySelector(".top-banner input");
-    const msg = document.querySelector(".top-banner .msg");
-    const list = document.querySelector(".ajax-section .cities");
-    const apiKey = "4d8fb5b93d4af21d66a2948710284366";
+//     /* SEARCH BY USING A CITY NAME OR A COMMA-SEPARATED CITY NAME ALONG WITH THE COUNTRY CODE */
+//     const form = document.querySelector(".top-banner form");
+//     const input = document.querySelector(".top-banner input");
+//     const msg = document.querySelector(".top-banner .msg");
+//     const list = document.querySelector(".ajax-section .cities");
+//     const apiKey = "4d8fb5b93d4af21d66a2948710284366";
 
-    form.addEventListener("submit", e => {
-        e.preventDefault();
+//     form.addEventListener("submit", e => {
+//         e.preventDefault();
 
-        let inputVal = input.value;
+//         let inputVal = input.value;
 
-        // check if there's already a city
-        const listItems = list.querySelectorAll(".ajax-section .city");
-        const listItemsArray = Array.from(listItems);
+//         // check if there's already a city
+//         const listItems = list.querySelectorAll(".ajax-section .city");
+//         const listItemsArray = Array.from(listItems);
 
-        if (listItemsArray.length > 0) {
-        const filteredArray = listItemsArray.filter(el => {
+//         if (listItemsArray.length > 0) {
+//         const filteredArray = listItemsArray.filter(el => {
 
-        let content = "";
-        if (inputVal.includes(",")) {
-        if (inputVal.split(",")[1].length > 2) {
-          inputVal = inputVal.split(",")[0];
-          content = el
-            .querySelector(".city-name span")
-            .textContent.toLowerCase();
-        } else {
-          content = el.querySelector(".city-name").dataset.name.toLowerCase();
-        }
-        } else {
-        content = el.querySelector(".city-name span").textContent.toLowerCase();
-        }
-      return content == inputVal.toLowerCase();
-    });
+//         let content = "";
+//         if (inputVal.includes(",")) {
+//         if (inputVal.split(",")[1].length > 2) {
+//           inputVal = inputVal.split(",")[0];
+//           content = el
+//             .querySelector(".city-name span")
+//             .textContent.toLowerCase();
+//         } else {
+//           content = el.querySelector(".city-name").dataset.name.toLowerCase();
+//         }
+//         } else {
+//         content = el.querySelector(".city-name span").textContent.toLowerCase();
+//         }
+//       return content == inputVal.toLowerCase();
+//     });
 
-    if (filteredArray.length > 0) {
-      msg.textContent = `You already know the weather for ${
-        filteredArray[0].querySelector(".city-name span").textContent
-      } ...otherwise be more specific by providing the country code as well`;
-      form.reset();
-      input.focus();
-      return;
-    }
-  }
-  weatherCard(form, input, msg, list, apiKey, inputVal);
-  });
-}
+//     if (filteredArray.length > 0) {
+//       msg.textContent = `You already know the weather for ${
+//         filteredArray[0].querySelector(".city-name span").textContent
+//       } ...otherwise be more specific by providing the country code as well`;
+//       form.reset();
+//       input.focus();
+//       return;
+//     }
+//   }
+//   weatherCard(form, input, msg, list, apiKey, inputVal);
+//   });
+// }
