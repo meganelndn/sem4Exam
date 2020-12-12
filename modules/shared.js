242a9eaafde21b635d9ae237882504d3586dada8
Patricia Bettio <pfbettio@gmail.com>
2020-12-12 16:41:17 +0100

    Cleanup newsletter and covid css and add Html secion for newsletter with post to restb.

--- modules/shared.js
+++ modules/shared.js
@@ -3,3 +3,19 @@ export async function fetchData(url, showSection) {
     const jsonData = await response.json();
     showSection(jsonData)
 }
+
+export function postNewsletter(formInfo) {
+    const postData = JSON.stringify(formInfo);
+    fetch("https://frontend2020-db3c.restdb.io/rest/hey-captain-newsletter", {
+            method: "post",
+            body: JSON.stringify(formInfo),
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+                "x-apikey": "5e95774d436377171a0c233c",
+                "cache-control": "no-cache",
+            },
+            body: postData,
+        })
+        .then((res) => res.json())
+        .then(console.log("Posted newsletter! - check RestDb"));
+}
